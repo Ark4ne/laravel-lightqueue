90e76e079a6017e51343ed1218ceadeef47ab86e
Ark4ne <gui.allegret@gmail.com>
2015-07-07 01:27:14 +0200

    Various fix

--- tests/FileQueueTest.php
+++ tests/FileQueueTest.php
@@ -8,6 +8,16 @@
 
 use Ark4ne\LightQueue\FileQueue;
 
+if(!function_exists('storage_path')){
+    function storage_path(){
+        return '';
+    }
+}
+if(!function_exists('base_path')){
+    function base_path(){
+        return '';
+    }
+}
 class FileQueueTest extends PHPUnit_Framework_TestCase
 {
 
